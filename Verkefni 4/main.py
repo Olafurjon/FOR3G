01eaa6b7b9e46bd3e56674e20289c97a4f7f8724
Olafurjon <olafurjonb@hotmail.com>
2017-03-30 21:40:48 +0000

    enemy og hero komnir með hreyfigetu

--- Verkefni 4/main.py
+++ Verkefni 4/main.py
@@ -21,6 +21,7 @@ clock = pygame.time.Clock()
 fighters = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
+tiefighters = list()
 space = pygame.image.load('img/space1.jpg')
 def mainmenu(gameDisplay,resW):
     myfont = pygame.font.SysFont("impact",48)
@@ -28,31 +29,37 @@ def mainmenu(gameDisplay,resW):
     pygame.draw.rect(gameDisplay, (255, 255, 255), (resW / 4, 240, 300, 100))
     gameDisplay.blit(playgametext, (resW / 3, 260))
 
+def readyfighters():
+    x = 50
+    y = 20
+    k = 0
+    for i in range(40):
+        tie = sprites.Enemy(20,15,resW,resH)
+        tiefighters.append(tie)
+        tie.rect.x = x
+        tie.rect.y = y
+        fighters.add(tie)
+        all_sprites.add(tie)
+        x += 50
+        k += 1
+        if k == 10:
+            y += 50
+            x = 50
+            k = 0
 
-x = 50
-y = 50
-k = 0
-for i in range(40):
-    tie = sprites.Enemy(20,15)
-    tie.rect.x = x
-    tie.rect.y = y
-    fighters.add(tie)
-    all_sprites.add(tie)
-    x += 50
-    k += 1
-    if k == 10:
-        y += 50
-        x = 50
-        k = 0
+readyfighters()
 
-
-
+Hero = sprites.Hero(50,50,resW,resH)
+hero = pygame.sprite.GroupSingle()
+all_sprites.add(Hero)
+Hero.rect.x = 300
+Hero.rect.y = 850
 
 
 
 started  = False
 crashed = False
-
+arg = "right"
 x= 0
 while not crashed:
     clock.tick(60)
@@ -63,7 +70,18 @@ while not crashed:
     gameDisplay.blit(space, (0, x - resH))
     if started:
         all_sprites.draw(gameDisplay)
-        all_sprites
+
+        if tiefighters[9].rect.x >= resW-30:
+            arg = "left"
+            for tie in fighters:
+                tie.move("down")
+        if tiefighters[0].rect.x <= 10:
+            arg = "right"
+            for tie in fighters:
+                tie.move("down")
+        for tie in fighters:
+            tie.move(arg)
+
         pygame.display.update()
         pygame.display.flip()
     if not started:
@@ -73,6 +91,18 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+
+    if event.type == pygame.KEYDOWN or pygame.KEYUP:  # Move the player if an arrow key is pressed
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            Hero.move(-2, 0)
+        if key[pygame.K_RIGHT]:
+            Hero.move(2, 0)
+        if key[pygame.K_UP]:
+            Hero.move(0, -2)
+        if key[pygame.K_DOWN]:
+            Hero.move(0, 2)
+
         if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT_BUTTON:
             if playgameclick.collidepoint(event.pos):
                 playgameclick.x = -1000000
@@ -83,18 +113,7 @@ while not crashed:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
         pygame.display.flip()
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
 quit()
--- Verkefni 4/sprites.py
+++ Verkefni 4/sprites.py
@@ -2,16 +2,43 @@ import pygame
 
 class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self,width,height):
+    def __init__(self,width,height,resw,resh):
         super(Enemy,self).__init__()
 
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('img/Tiefightersmall.png').convert_alpha()
         self.rect = self.image.get_rect()
+    def move(self,arg):
+        if arg == "right":
+            self.rect.x += 1
+        if arg == "left":
+            self.rect.x -= 1
+        if arg == "down":
+            self.rect.y += 10
 
-    def update(self,k,resw,resh):
-        self.rect.x += k
-        k += 1
+class Hero(pygame.sprite.Sprite):
+
+    def __init__(self,width,height,resw,resh):
+        super(Hero,self).__init__()
+
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/serenitysmall.png')
+        self.rect = self.image.get_rect()
+
+    def move(self, dx, dy):
+        # Move each axis separately. Note that this checks for collisions both times.
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+
+    def move_single_axis(self, dx, dy):
+        # Move the rectangle
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+
 
 
 
