577d2bad71caf581e6389e815bf26d4d6d4db14a
Olafurjon <olafurjonb@hotmail.com>
2017-04-03 11:10:29 +0000

    er að skoða hvernig á að gera snyrtilegra og meira smooth

--- Verkefni 4/main.py
+++ Verkefni 4/main.py
@@ -4,6 +4,9 @@
 
 import pygame
 import sprites
+import time
+import random
+from math import floor
 
 
 pygame.init()
@@ -17,24 +20,25 @@ myfont = pygame.font.SysFont("impact",48)
 gameDisplay = pygame.display.set_mode((resW,resH))
 pygame.display.set_caption("Verkefni 4")
 clock = pygame.time.Clock()
-
+r = random.randint(15, 50)
 fighters = pygame.sprite.Group()
-clip = list()
+clip = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
 tiefighters = list()
+wave2 = pygame.sprite.Group()
 space = pygame.image.load('img/space1.jpg')
 def mainmenu(gameDisplay,resW):
     myfont = pygame.font.SysFont("impact",48)
     playgametext = myfont.render("Play Game",1,(0,0,0))
     pygame.draw.rect(gameDisplay, (255, 255, 255), (resW / 4, 240, 300, 100))
     gameDisplay.blit(playgametext, (resW / 3, 260))
-
+spawntime = 10
 def readyfighters():
     x = 50
-    y = 20
+    y = -300
     k = 0
-    for i in range(40):
+    for i in range(r):
         tie = sprites.Enemy(20,15,resW,resH)
         tiefighters.append(tie)
         tie.rect.x = x
@@ -48,53 +52,126 @@ def readyfighters():
             x = 50
             k = 0
 
+def fighters2():
+    x = 50
+    y = -350
+    k = 0
+    for i in range(10):
+        newtie = sprites.Enemy(20,15,resW,resH)
+        tiefighters.append(newtie)
+        newtie.rect.x = x
+        newtie.rect.y = y
+        wave2.add(newtie)
+        all_sprites.add(newtie)
+        x += 50
+        k += 2
+        if k == 10:
+            y += 50
+            x = 50
+            k = 0
+
 readyfighters()
+
+
 
 Hero = sprites.Hero(50,50,resW,resH)
 hero = pygame.sprite.GroupSingle()
+bullet = sprites.bullets(10, 10,Hero)
 all_sprites.add(Hero)
 Hero.rect.x = 300
 Hero.rect.y = 850
-
-
-
+missile = 0
+shoot = True
 started  = False
 crashed = False
 arg = "right"
+t0 = time.time()
 x= 0
+t = 0
+k = 1
 while not crashed:
-    clock.tick(60)
-    x += 1
     if x == resH:
         x = 0
+    x+= 1
     gameDisplay.blit(space, (0, x))
     gameDisplay.blit(space, (0, x - resH))
+    clock.tick(60)
+    t1 = time.time()
+    time_Start = True
+    ms = t1 - t0
+    sek = int(floor(ms))
+    print sek
+    if sek == spawntime:
+        readyfighters()
+        t0 = time.time()
+        t += 1
+        if t == 5:
+            if k != 2:
+                k += 1
+            if k > 2:
+                k == 2
+            if k == 2:
+                spawntime = 20
+
+
+            t = 0
+
+
     if started:
+
         all_sprites.draw(gameDisplay)
-        if tiefighters[9].rect.x >= resW-30:
-            arg = "left"
-            for tie in fighters:
-                tie.move("down")
-        if tiefighters[0].rect.x <= 10:
-            arg = "right"
-            for tie in fighters:
-                tie.move("down")
 
         for tie in fighters:
-            tie.move(arg)
-            if pygame.sprite.spritecollideany(tie,clip) and clip.__len__() != 0:
-                fighters.remove(tie)
-                all_sprites.remove_internal(tie)
-                all_sprites.remove(bullet)
+           if tie.rect.x >= (resW-30):
+               print "snúavið"
+               arg = "left"
+               for tie in fighters:
+                   tie.move("down",k)
+
+           if tie.rect.x <= 0:
+               arg = "right"
+               print "snúavið"
+               for tie in fighters:
+                   tie.move("down",k)
+
+        for newtie in wave2:
+           if newtie.rect.x >= (resW-30):
+               arg = "leftska"
+
+
+           if newtie.rect.x <= 0:
+               arg = "skaright"
 
 
         if clip.__len__() !=  0:
-            for bullet in clip:
-                bullet.rect.y -= 5
-                if bullet.rect.y <= 0 or bullet.rect.y >= 900:
-                    clip.remove(bullet)
-                    print "bullet dead"
-                    all_sprites.remove(bullet)
+           for bullet in clip:
+               bullet.rect.y -= 5
+               if bullet.rect.y <= 0 or bullet.rect.y >= 900:
+                   clip.remove(bullet)
+                   print "bullet dead"
+                   all_sprites.remove(bullet)
+
+        for tie in fighters:
+            tie.move(arg,k)
+            if pygame.sprite.spritecollideany(tie, clip):
+                for bullet in clip:
+                    if bullet.rect.colliderect(tie):
+                        all_sprites.remove(bullet)
+                        clip.remove(bullet)
+                all_sprites.remove(tie)
+                fighters.remove(tie)
+
+        for newtie in wave2:
+            newtie.move(arg,k)
+            if pygame.sprite.spritecollideany(newtie, clip):
+                for bullet in clip:
+                    if bullet.rect.colliderect(newtie):
+                        all_sprites.remove(bullet)
+                        clip.remove(bullet)
+                all_sprites.remove(newtie)
+                wave2.remove(newtie)
+
+
 
         pygame.display.update()
         pygame.display.flip()
@@ -106,7 +183,7 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-
+    missile += 1
     if event.type == pygame.KEYDOWN or pygame.KEYUP:  # Move the player if an arrow key is pressed
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
@@ -117,21 +194,26 @@ while not crashed:
             Hero.move(0, -2)
         if key[pygame.K_DOWN]:
             Hero.move(0, 2)
-        if key[pygame.K_SPACE]:
-            bullet = sprites.bullets(10, 10)
-            bullet.rect.y = Hero.rect.y - 10
-            bullet.rect.x = Hero.rect.x
+        if missile >= 20:
+            missile = 0
+
+        if key[pygame.K_SPACE] and missile == 0 :
+            print "shoot"
+            bullet = sprites.bullets(10, 10,Hero)
             all_sprites.add(bullet)
-            clip.append(bullet);
+            clip.add(bullet)
 
 
-        if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT_BUTTON:
-            if playgameclick.collidepoint(event.pos):
-                playgameclick.x = -1000000
-                playgameclick.y = -1000000
-                gameDisplay.blit(space, (0, x))
-                gameDisplay.blit(space, (0, x - resH))
-                started = True
+
+
+
+    if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT_BUTTON:
+        if playgameclick.collidepoint(event.pos):
+            playgameclick.x = -1000000
+            playgameclick.y = -1000000
+            gameDisplay.blit(space, (0, x))
+            gameDisplay.blit(space, (0, x - resH))
+            started = True
 
 
 
--- Verkefni 4/sprites.py
+++ Verkefni 4/sprites.py
@@ -4,25 +4,37 @@ class Enemy(pygame.sprite.Sprite):
 
     def __init__(self,width,height,resw,resh):
         super(Enemy,self).__init__()
-
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('img/Tiefightersmall.png').convert_alpha()
         self.rect = self.image.get_rect()
-    def move(self,arg):
+
+
+    def move(self,arg,k):
         if arg == "right":
-            self.rect.x += 1
+            self.rect.x += k
         if arg == "left":
+            self.rect.x -= k
+        if arg == "leftska":
             self.rect.x -= 1
+            self.rect.y += 1
+        if arg == "rightska":
+            self.rect.x += 1
+            self.rect.y += 1
         if arg == "down":
-            self.rect.y += 10
+            i = 0
+            while i < 10:
+                self.rect.y += 1
+                i+=1
 
 class bullets(pygame.sprite.Sprite):
-    def __init__(self,width,height):
+    def __init__(self,width,height,Hero):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/serenitysmall.png')
+        self.image = pygame.image.load('img/skot1.png')
         self.rect = self.image.get_rect()
-        self.rect.x = -100
-        self.rect.y = -100
+        self.rect.y = Hero.rect.y - 27
+        self.rect.x = Hero.rect.x + 12
+
+
 
 
 
@@ -35,7 +47,9 @@ class Hero(pygame.sprite.Sprite):
         self.image = pygame.image.load('img/serenitysmall.png')
         self.rect = self.image.get_rect()
 
+
     def move(self, dx, dy):
+
         # Move each axis separately. Note that this checks for collisions both times.
         if dx != 0:
             self.move_single_axis(dx, 0)
@@ -46,10 +60,14 @@ class Hero(pygame.sprite.Sprite):
         # Move the rectangle
         self.rect.x += dx
         self.rect.y += dy
-
-    def shoot(self,resH):
-        if pygame.event == pygame.KEYUP or pygame.KEYDOWN:
-            key = pygame.key.get_pressed()
+        if self.rect.y <= 0:
+            self.rect.y = 0
+        if self.rect.y >= 850:
+            self.rect.y = 850
+        if self.rect.x >= 600:
+            self.rect.x = -30
+        if self.rect.x <= -40:
+            self.rect.x = 580
 
 
 
